76e4f06163e3479b27f3d8347f1e14bda6330d2b
geovanifg <geofergon@msn.com>
2018-10-18 10:45:56 -0300

    a

---
+++ Tupy.Logger.Providers.LiteDB/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tupy.Logger.Providers.LiteDB
+{
+    public class Class1
+    {
+    }
+}
